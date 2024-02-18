65b742bc4a01573ba45162ccd6bda60d7967fe6d
Javid <javid.momeni@gmail.com>
2024-02-18 17:25:39 +0330

    build

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,53 @@
-import { Button, Paper, Title, useMantineTheme, Text, Group } from '@mantine/core'
-import './App.css'
-import { Carousel } from '@mantine/carousel'
-import '@mantine/carousel/styles.css'
-import '@mantine/carousel/styles.layer.css'
-import { useMediaQuery } from '@mantine/hooks';
-
+import {
+  Button,
+  Paper,
+  Title,
+  useMantineTheme,
+  Text,
+  Group,
+} from "@mantine/core";
+import "./App.css";
+import { Carousel } from "@mantine/carousel";
+import "@mantine/carousel/styles.css";
+import "@mantine/carousel/styles.layer.css";
+import { useMediaQuery } from "@mantine/hooks";
 
 const data = [
   {
     image:
-      'https://images.unsplash.com/photo-1508193638397-1c4234db14d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-    title: 'Best forests to visit in North America',
-    category: 'nature',
+      "https://images.unsplash.com/photo-1508193638397-1c4234db14d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+    title: "Best forests to visit in North America",
+    category: "nature",
   },
   {
     image:
-      'https://images.unsplash.com/photo-1559494007-9f5847c49d94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-    title: 'Hawaii beaches review: better than you think',
-    category: 'beach',
+      "https://images.unsplash.com/photo-1559494007-9f5847c49d94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+    title: "Hawaii beaches review: better than you think",
+    category: "beach",
   },
   {
     image:
-      'https://images.unsplash.com/photo-1608481337062-4093bf3ed404?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-    title: 'Mountains at night: 12 best locations to enjoy the view',
-    category: 'nature',
+      "https://images.unsplash.com/photo-1608481337062-4093bf3ed404?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+    title: "Mountains at night: 12 best locations to enjoy the view",
+    category: "nature",
   },
   {
     image:
-      'https://images.unsplash.com/photo-1507272931001-fc06c17e4f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-    title: 'Aurora in Norway: when to visit for best experience',
-    category: 'nature',
+      "https://images.unsplash.com/photo-1507272931001-fc06c17e4f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+    title: "Aurora in Norway: when to visit for best experience",
+    category: "nature",
   },
   {
     image:
-      'https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-    title: 'Best places to visit this winter',
-    category: 'tourism',
+      "https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+    title: "Best places to visit this winter",
+    category: "tourism",
   },
   {
     image:
-      'https://images.unsplash.com/photo-1582721478779-0ae163c05a60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-    title: 'Active volcanos reviews: travel at your own risk',
-    category: 'nature',
+      "https://images.unsplash.com/photo-1582721478779-0ae163c05a60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+    title: "Active volcanos reviews: travel at your own risk",
+    category: "nature",
   },
 ];
 
@@ -82,24 +88,24 @@ function App() {
       <Card {...item} />
     </Carousel.Slide>
   ));
-   return (
+  return (
     <>
       <Carousel
-      slideSize={{ base: '100%', sm: '50%' }}
-      slideGap={{ base: 'xl', sm: 2 }}
-      align="start"
-      slidesToScroll={mobile ? 1 : 2}
-    >
-      {slides}
-    </Carousel>
-    <Group>
+        slideSize={{ base: "100%", sm: "50%" }}
+        slideGap={{ base: "xl", sm: 2 }}
+        align="start"
+        slidesToScroll={mobile ? 1 : 2}
+      >
+        {slides}
+      </Carousel>
+      <Group>
         <Button color="ocean-blue">Ocean blue button</Button>
         <Button color="bright-pink" variant="filled">
           Bright pink button
         </Button>
       </Group>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import '@mantine/core/styles.css';
-import { MantineProvider, createTheme } from '@mantine/core';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "@mantine/core/styles.css";
+import { MantineProvider, createTheme } from "@mantine/core";
 const theme = createTheme({
-  fontFamily: 'Open Sans, sans-serif',
-  primaryColor: 'cyan',
+  fontFamily: "Open Sans, sans-serif",
+  primaryColor: "cyan",
   colors: {
-    'ocean-blue': ['#7AD1DD', '#5FCCDB', '#44CADC', '#2AC9DE', '#1AC2D9', '#11B7CD', '#09ADC3', '#0E99AC', '#128797', '#147885'],
-    'bright-pink': ['#F0BBDD', '#ED9BCF', '#EC7CC3', '#ED5DB8', '#F13EAF', '#F71FA7', '#FF00A1', '#E00890', '#C50E82', '#AD1374'],
+    "ocean-blue": [
+      "#7AD1DD",
+      "#5FCCDB",
+      "#44CADC",
+      "#2AC9DE",
+      "#1AC2D9",
+      "#11B7CD",
+      "#09ADC3",
+      "#0E99AC",
+      "#128797",
+      "#147885",
+    ],
+    "bright-pink": [
+      "#F0BBDD",
+      "#ED9BCF",
+      "#EC7CC3",
+      "#ED5DB8",
+      "#F13EAF",
+      "#F71FA7",
+      "#FF00A1",
+      "#E00890",
+      "#C50E82",
+      "#AD1374",
+    ],
   },
 });
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <MantineProvider theme={theme}>
       <App />
     </MantineProvider>
   </React.StrictMode>,
-)
+);

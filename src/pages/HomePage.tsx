ea87afb6005f7b99018a5b60f7a68cf670009eaa
Javid <javid.momeni@gmail.com>
2024-02-20 16:30:38 +0330

    build

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,25 +6,25 @@ const data = [
     image:
       "https://images.unsplash.com/photo-1508193638397-1c4234db14d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
     title: "Best forests to visit in North America",
-    category: "nature",
+    category: "آب",
   },
   {
     image:
       "https://images.unsplash.com/photo-1559494007-9f5847c49d94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
     title: "Hawaii beaches review: better than you think",
-    category: "beach",
+    category: "باد",
   },
   {
     image:
       "https://images.unsplash.com/photo-1608481337062-4093bf3ed404?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
     title: "Mountains at night: 12 best locations to enjoy the view",
-    category: "nature",
+    category: "خاک",
   },
   {
     image:
       "https://images.unsplash.com/photo-1507272931001-fc06c17e4f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
     title: "Aurora in Norway: when to visit for best experience",
-    category: "nature",
+    category: "آتش",
   },
 ];
 
@@ -43,6 +43,7 @@ function Card({ image, title, category }: CardProps) {
       style={{ backgroundImage: `url(${image})` }}
       className="card"
       m={10}
+      h={"300"}
     >
       <div>
         <Text className="category" size="xs">
@@ -53,7 +54,7 @@ function Card({ image, title, category }: CardProps) {
         </Title>
       </div>
       <Button variant="white" color="dark">
-        Read article
+        ادامه
       </Button>
     </Paper>
   );

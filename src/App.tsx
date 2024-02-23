d012810dc838af4d215c06fb67fc4f98287731ed
Javid <javid.momeni@gmail.com>
2024-02-23 19:52:25 +0330

    build

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import HomePage from "./pages/HomePage";
 import { Route, Routes } from "react-router-dom";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-
+import { TouchBackend } from "react-dnd-touch-backend";
 import ProfilePage from "./pages/ProfilePage";
 import GamePage from "./pages/GamePage";
 import HelpPage from "./pages/HelpPage";
@@ -10,6 +10,7 @@ import InvitePage from "./pages/InvitePage";
 import RankingPage from "./pages/RankingPage";
 import ElementsPage from "./pages/ElementsPage";
 import WebApp from "@twa-dev/sdk";
+import { useOs } from "@mantine/hooks";
 function App() {
   WebApp.ready();
   WebApp.expand();
@@ -17,8 +18,12 @@ function App() {
   WebApp.setHeaderColor("#0e87cc");
   WebApp.enableClosingConfirmation;
   WebApp.BackButton.show();
+  const os = useOs();
+  const isMobile = os === "ios" || os === "android";
+  const backend = isMobile ? TouchBackend : HTML5Backend;
+
   return (
-    <DndProvider backend={HTML5Backend}>
+    <DndProvider backend={backend}>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/profile" element={<ProfilePage />} />

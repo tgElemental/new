93ec520cd619412c0dfd5c0ecc935a22e05c80d2
Javid <javid.momeni@gmail.com>
2024-02-27 16:09:31 +0330

    build

--- src/pages/RankingPage.tsx
+++ src/pages/RankingPage.tsx
@@ -80,7 +80,23 @@ const RankingPage = () => {
   };
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <>
+        <SimpleGrid cols={3} p={"xs"}>
+          <Skeleton height={150} width={"100%"} mb="xl" />
+          <Skeleton height={150} width={"100%"} mb="xl" />
+          <Skeleton height={150} width={"100%"} mb="xl" />
+        </SimpleGrid>
+        <Center>
+          <Loader color="blue" size="xl" type="dots" />
+        </Center>
+        <Skeleton height={20} mt={10} p={10} width="100%" radius="xs" />
+        <Skeleton height={20} mt={10} p={10} width="100%" radius="xs" />
+        <Skeleton height={20} mt={10} p={10} width="100%" radius="xs" />
+        <Skeleton height={20} mt={10} p={10} width="100%" radius="xs" />
+        <Skeleton height={20} mt={10} p={10} width="100%" radius="xs" />
+      </>
+    );
   }
 
   const topTreeUsers = getTopThree(treeData);

7fe2affa2a627adc58e8f273df6697f4690fa57b
Javid <javid.momeni@gmail.com>
2024-02-27 13:20:30 +0330

    build

--- src/components/GameMessage.tsx
+++ src/components/GameMessage.tsx
@@ -2,7 +2,18 @@
 import { Text, Timeline } from "@mantine/core";
 import { IconGitBranch } from "@tabler/icons-react";
 import React from "react";
-
+import data from "@emoji-mart/data";
+import { init } from "emoji-mart";
+init({ data });
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      "em-emoji": any;
+    }
+  }
+}
 interface GameMessageProps {
   elementName: string;
   botElementName: string;
@@ -11,7 +22,26 @@ interface GameMessageProps {
   remain: number;
   text: string;
 }
+interface EmojiMap {
+  [key: string]: JSX.Element;
+}
 
+const elementToEmojiMap: EmojiMap = {
+  آب: <em-emoji id="droplet" Size="2em"></em-emoji>,
+  باد: <em-emoji id="wind_blowing_face" Size="2em"></em-emoji>,
+  خاک: <em-emoji id="large_brown_circle" Size="2em"></em-emoji>,
+  آتش: <em-emoji id="fire" Size="2em"></em-emoji>,
+  هوا: <em-emoji id="wind_blowing_face" Size="2em"></em-emoji>,
+  درخت: <em-emoji id="deciduous_tree" Size="2em"></em-emoji>,
+  نور: <em-emoji id="sunny" Size="2em"></em-emoji>,
+};
+const renderEmoji = (elementName: string) => {
+  return (
+    elementToEmojiMap[elementName] || (
+      <em-emoji id="question_mark" Size="2em"></em-emoji>
+    )
+  ); // Fallback to a question mark emoji if the element name is not found
+};
 const GameMessage: React.FC<GameMessageProps> = ({
   elementName,
   botElementName,
@@ -23,12 +53,12 @@ const GameMessage: React.FC<GameMessageProps> = ({
   return (
     <>
       <Timeline color="green" active={3} lineWidth={3} bulletSize={40}>
-        <Timeline.Item bullet={<IconGitBranch size={12} />} title="عنصر شما">
+        <Timeline.Item bullet={renderEmoji(elementName)} title="عنصر شما">
           <Text c="dimmed" size="sm">
             {elementName}
           </Text>
         </Timeline.Item>
-        <Timeline.Item bullet={<IconGitBranch size={12} />} title="عنصر رقیب">
+        <Timeline.Item bullet={renderEmoji(botElementName)} title="عنصر رقیب">
           <Text c="dimmed" size="sm">
             {botElementName}
           </Text>
